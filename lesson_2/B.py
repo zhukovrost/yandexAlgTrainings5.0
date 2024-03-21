d5ef3d815b35a0938eee7e558da80ec18d33a534
Ростислав Жуков <rosf.zhukov@gmail.com>
2024-03-21 12:39:28 +0300

    Разбор второй лекции
    Задача B

--- lesson_2/B.py
+++ lesson_2/B.py
@@ -2,21 +2,13 @@ def main():
     # 14 test wrong answer
     n, k = map(int, input().split())
 
-    if n == 0 or n == 1 or k <= 0:
-        print(0)
-        return
-
     arr = list(map(int, input().split()))
     result = 0
 
-    if k >= n:
-        for i in range(n):
-            for j in range(i + 1, n):
-                result = max(result, arr[j] - arr[i])
-    else:
-        for i in range(n - k):
-            for j in range(i + 1, i + k + 1):
-                result = max(result, arr[j] - arr[i])
+    for i in range(n):
+        for j in range(k + 1):
+            if i + j < n:
+                result = max(result, arr[i + j] - arr[i])
 
     print(result)
 
